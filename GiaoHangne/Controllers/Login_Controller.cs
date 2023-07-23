5e19caf50c9a35b6d48039f64520b97d6aaf36f8
Hoddy0812 <thanhpham.15112003pl@gmail.com>
2023-07-23 15:41:05 +0700

    #

---
+++ GiaoHangne/Controllers/Login_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GiaoHangne.Controllers
+{
+    public class Login_Controller : Controller
+    {
+        // GET: Login_
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
